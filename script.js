f234707b9aeebf39c571d5ccc85295f43a35177a
Karolina Hoover <karolina.vitvytska@gmail.com>
2020-11-12 23:06:43 -0500

    newfetch

--- script.js
+++ script.js
@@ -1,52 +1,35 @@
+let body = document.body;
+let string = window.location.search;
+let url = 'https://api.github.com/users/karolinahoover';
 
-const url = 'https://api.github.com/users/';
- const login = 'karolinahoover';
- let currentDate = new Date();
+fetch(url)
+    .then(res => res.json())
+    .then(json => {
+        console.log(json.avatar_url);
+        console.log(json.name);
+        console.log(json.bio);
+        console.log(json.html_url);
 
- setTimeout(() => {
-   const preloader = document.getElementById('floatingCirclesG');
-   preloader.classList.add('stop');
- }, 3000);
+        let photo = new Image();
+        photo.src = json.avatar_url;
+        body.append(photo);
 
- const getDate = new Promise((resolve, reject) => {
-   setTimeout(() => currentDate ? resolve(currentDate) : reject('имя не найдено'), 3000);
- });
+        let name = document.createElement('p');
+        if (json.name != null) {
+            name.innerHTML = json.name;
+        } else {
+            name.innerHTML = 'Информация о пользователе недоступна';
+        }
+        body.append(name);
+        name.addEventListener("click", () => window.location = json.html_url);
 
- Promise.all([getDate])
-   .then(([currentDate]) => fetch(`${url}${login}`))
-   .then(res => res.json())
-   .then(json => {
-     console.log(json);
-     const user = {
-       avatar: json.avatar_url,
-       name: json.login,
-       bio: json.bio,
-       link: json.html_url
-     }
-     for (key in user) {
-       if (user[key] == null) {
-         let err = document.createElement('h2');
-         err.innerHTML = 'Информация недоступна(';
-         document.body.appendChild(err);
-         return;
-       }
-     }
-     let name = document.createElement('a');
-     name.innerHTML = user.name;
-     name.href = user.link;
-     document.body.appendChild(name);
+        let bio = document.createElement('p');
+        if (json.bio != null) {
+            bio.innerHTML = json.bio;
+        } else {
+            bio.innerHTML = 'Информация о пользователе недоступна';
+        }
+        body.append(bio);
 
-     let bio = document.createElement('p');
-     bio.innerHTML = user.bio;
-     document.body.appendChild(bio);
-
-     let avatar = document.createElement('img');
-     avatar.src = user.avatar;
-     avatar.classList.add('avatar');
-     document.body.appendChild(avatar);
-
-     let date = document.createElement('p');
-     date.innerHTML = currentDate;
-     document.body.appendChild(date);
-   })
-   .catch(err => console.log(err));
+    })
+    .catch(err => alert('Информация о пользователе недоступна'));
